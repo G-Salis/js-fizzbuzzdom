f3daa0fd32f3754d09ae70a4229f90943ab75d6a
G-Salis <giuseppesalislavoro@gmail.com>
2021-10-25 16:05:58 +0200

    First Commit + Esercizio finito

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const row = document.querySelector(".row");
+
+for (let i = 1; i < 100; i++) {
+  const box = document.createElement("div");
+  box.classList.add("box");
+  row.append(box);
+
+  if (!(i % 5) && !(i % 3)){
+    box.append("fizzbuzz");
+    box.classList.add("boxred");
+
+  }else if(!(i % 3)){
+    box.append("fizz");
+    box.classList.add("boxgreen");
+
+  }else if(!(i % 5)){
+    box.append("buzz");
+    box.classList.add("boxorange");
+
+  }else{
+    box.append(i);
+  }
+
+}
